3054e886cb6162f4ffe60d8a53719bc14da30b86
kokohany655 <kokoh2370@gmail.com>
2023-07-14 13:01:00 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,10 @@ int main(void)
 	{
 		if (i == 9)
 			putchar(i + '0');
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		else
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
 	}
 	putchar('\n');
 return (0);
