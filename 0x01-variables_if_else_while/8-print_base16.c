f094a411247d283d311b269d4c6082b0435e11b6
kokohany655 <kokoh2370@gmail.com>
2023-07-14 12:45:29 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print hexadecimenl
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+		putchar(i + '0');
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+return (0);
+}
